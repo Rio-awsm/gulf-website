f0c52199d749a6fd908eb4b8bf11b1064c6a5391
Supriyo Saha <mm.saha1982@gmail.com>
2024-10-04 02:09:51 +0530

    team images

--- src/pages/AboutUs/Leadership.jsx
+++ src/pages/AboutUs/Leadership.jsx
@@ -1,24 +1,114 @@
 import React from "react";
-import pic from "/assets/team.webp"; 
+// import pic from "/assets/team.webp";
 
 const Leadership = () => {
   const teamMembers = [
-    { name: "LOREM IPSUM", designation: "Designation" },
-    { name: "LOREM IPSUM", designation: "Designation" },
-    { name: "LOREM IPSUM", designation: "Designation" },
-    { name: "LOREM IPSUM", designation: "Designation" },
-    { name: "LOREM IPSUM", designation: "Designation" },
-    { name: "LOREM IPSUM", designation: "Designation" },
-    { name: "LOREM IPSUM", designation: "Designation" },
-    { name: "LOREM IPSUM", designation: "Designation" },
-    { name: "LOREM IPSUM", designation: "Designation" },
+    {
+      name: "LOREM IPSUM",
+      designation: "designation",
+      image: "/assets/team1.png",
+    },
+    {
+      name: "Maximilien Photiou",
+      designation: "Property Advisor",
+      image: "/assets/team2.png",
+    },
+    {
+      name: "Nikhil suvarna",
+      designation: "Sales Manager",
+      image: "/assets/team3.png",
+    },
+    {
+      name: "LOREM IPSUM",
+      designation: "designation",
+      image: "/assets/team4.png",
+    },
+    {
+      name: "LOREM IPSUM",
+      designation: "designation",
+      image: "/assets/team5.png",
+    },
+    {
+      name: "Anna Zhurlova",
+      designation: "designation",
+      image: "/assets/team6.png",
+    },
+    {
+      name: "Ahmad Alhamad",
+      designation: "Property Advisor",
+      image: "/assets/team7.png",
+    },
+    {
+      name: "LOREM IPSUM",
+      designation: "designation",
+      image: "/assets/team8.png",
+    },
+    {
+      name: "LOREM IPSUM",
+      designation: "designation",
+      image: "/assets/team9.png",
+    },
+    {
+      name: "Nailia Gaisina",
+      designation: "Property Advisor",
+      image: "/assets/team10.png",
+    },
+    {
+      name: "LOREM IPSUM",
+      designation: "designation",
+      image: "/assets/team11.png",
+    },
+    {
+      name: "LOREM IPSUM",
+      designation: "designation",
+      image: "/assets/team12.png",
+    },
+    {
+      name: "LOREM IPSUM",
+      designation: "designation",
+      image: "/assets/team13.png",
+    },
+    {
+      name: "LOREM IPSUM",
+      designation: "designation",
+      image: "/assets/team14.png",
+    },
+    {
+      name: "Maria Matos",
+      designation: "Property Advisor",
+      image: "/assets/team15.png",
+    },
+    {
+      name: "Neha Sharma",
+      designation: "Business Development",
+      image: "/assets/team16.png",
+    },
+    {
+      name: "LOREM IPSUM",
+      designation: "designation",
+      image: "/assets/team17.png",
+    },
+    {
+      name: "LOREM IPSUM",
+      designation: "designation",
+      image: "/assets/team18.png",
+    },
+    {
+      name: "Ariana Versace",
+      designation: "Property Advisor",
+      image: "/assets/team19.png",
+    },
   ];
 
   return (
     <div className="lg:px-40 px-8">
       <div className=" font-bebas lg:mb-8 ">
-        <h2 className="lg:text-8xl text-6xl tracking-wider text-[#024959] lg:ml-[10%]">LEADERSHIP</h2>
-        <p className="lg:text-6xl text-4xl tracking-wider text-[#F2762E] mt-2 ml-[30%] lg:ml-[25%]">MEET OUR TEAM</p>
+        <h2 className="lg:text-8xl text-6xl tracking-wider text-[#024959] lg:ml-[10%]">
+          LEADERSHIP
+        </h2>
+        <p className="lg:text-6xl text-4xl tracking-wider text-[#F2762E] mt-2 ml-[30%] lg:ml-[25%]">
+          MEET OUR TEAM
+        </p>
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
@@ -28,7 +118,7 @@ const Leadership = () => {
             className="rounded-lg p-6 flex flex-col items-center justify-between"
           >
             <img
-              src={pic}
+              src={member.image}
               alt={member.name}
               className="w-[90%] h-auto object-cover mb-4"
             />
